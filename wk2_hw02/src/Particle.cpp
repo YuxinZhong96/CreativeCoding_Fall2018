d79cc36d213ed97213644af72f0465c9ddfb93bf
Stella <zhony815@newschool.edu>
2018-09-14 09:03:38 +0200

    wk2

---
+++ wk2_hw02/src/Particle.cpp
@@ -0,0 +1,8 @@
+//
+//  Particle.cpp
+//  wk2_hw02
+//
+//  Created by Zhong Yuxin on 13/09/2018.
+//
+
+#include "Particle.hpp"
---
+++ wk2_hw02/src/Particle.hpp
@@ -0,0 +1,13 @@
+//
+//  Particle.hpp
+//  wk2_hw02
+//
+//  Created by Zhong Yuxin on 13/09/2018.
+//
+
+#ifndef Particle_hpp
+#define Particle_hpp
+
+#include <stdio.h>
+
+#endif /* Particle_hpp */
